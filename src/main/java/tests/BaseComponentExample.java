84def3344c1f365fff439c71064ed59baaf474df
Alexandra <cernea.alexandra279@gmail.com>
2024-07-17 19:04:42 +0300

    reparare test

--- src/main/java/tests/BaseComponentExample.java
+++ src/main/java/tests/BaseComponentExample.java
@@ -2,18 +2,24 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.Test;
+
 import io.restassured.response.Response;
 import testData.DataBuilder;
 import utils.BaseComponent;
 
 public class BaseComponentExample extends BaseComponent {
-
+	
+	
 	@Test(priority=1)
 	public void createNewUser() {
 		
-		Response response = doPostReqest("api/Users",DataBuilder.buildUser().toJSONString(),200);
+		Response response = doPostReqest("api/users", 
+										DataBuilder.buildUser().toJSONString(), 
+										201);
 		
-		assertEquals(response.jsonPath().getString("success"),"true");
+		assertEquals(response.jsonPath().getString("success"), "true");
+		
 	}
-	
+
 }
--- src/main/java/tests/RestExample.java
+++ src/main/java/tests/RestExample.java
@@ -1,54 +1,61 @@
 package tests;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import com.github.javafaker.Faker;
 
-
-import org.json.simple.JSONObject;
-
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 
-import java.io.File;
 public class RestExample {
-
+	
 	@SuppressWarnings("unchecked")
 	@Test
 	public void restExampleTest() {
 		
-		Faker fakedata = new Faker();
+		Faker fakeData = new Faker();
+		
+		
 		JSONObject requestBody = new JSONObject();
-		requestBody.put("name",fakedata.artist().name());
-		requestBody.put("email",fakedata.internet().emailAddress());
-		requestBody.put("age","23");
-		requestBody.put("gender","m");
-//		
-		File fisier = new File("data.json");
+		requestBody.put("name", fakeData.artist().name());
+		requestBody.put("email", fakeData.internet().emailAddress());
+		requestBody.put("age", "45");
+		requestBody.put("gender", "m");
 
+		File fisier =  new File("data.json");
+		
 		Response response = given()
-				.header("Content-Type","application/json")
-				//ex1-->direct in body as string
-			/*	.body("{\r\n"
-						+ "    \"name\": \"nume\",\r\n"
-						+ "    \"email\": \"em3ail@email.com\",\r\n"
-						+ "    \"age\": \"13\",\r\n"
-						+ "    \"gender\": \"M\"\r\n"
-						+ "}") */
-				//ex2 --> body as JsonObject
-				.body(requestBody.toJSONString()) 
-				//.body(fisier) -->optiunea 3
+				.headers("Content-Type", "application/json")
+				//ex 1 --> direct in body as String
+				/*.body("{\n"
+							+ "    \"name\":\"Test\",\n"
+							+ "    \"email\":\"test@test123334.com\",\n"
+							+ "    \"age\":\"33\",\n"
+							+ "    \"gender\":\"m\"\n"
+							+ "    }") */
+				//ex 2 --> body as JsonObject
+				//.body(requestBody.toJSONString())
+				//ex 3 --> body as file
+				.body(fisier)
 				.when()
-				.post("https://keytrcrud.herokuapp.com/api/users/")
+					.post("https://keytrcrud.herokuapp.com/api/users/")
 				.then()
 				.assertThat().statusCode(201).extract().response();
 		
+		System.out.println(response);
 		System.out.println(response.asString());
 		System.out.println(response.asPrettyString());
-		String msg =  response.jsonPath().getString("msg");
-		System.out.println(msg);
+		String msg = response.jsonPath().getString("msg");
+		assertEquals(msg, "Successfully added!");
 		String email = response.jsonPath().getString("result.email");
 		System.out.println(email);
-		
 	}
+	
+
 }
